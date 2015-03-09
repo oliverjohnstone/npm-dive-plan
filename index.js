1d3afd58908abb8c0bd1629e9d3af04b7cb894c4
Oliver Johnstone <oliver.johnstone@clock.co.uk>
2015-03-09 09:30:17 +0000

    Fixing bugs in calculations

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports =
   { DiveProfile: require('./lib/DiveProfile')
   , WayPoint: require('./lib/WayPoint')
+  // This should be moved out into its own module
+  , Gas: require('npm-buhlmann-ZH-L16').Gas
   }
